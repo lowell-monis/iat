2f0ec1b9b4c7b08558bf8ecf4ac61f277cc0de72
lowell-monis <monislow@msu.edu>
2026-05-30 18:25:13 -0700

    experimenting/testing stimuli size and positioning

--- study2-gender-disability/genderiat.js
+++ study2-gender-disability/genderiat.js
@@ -18,7 +18,7 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/iat10
                 { image: 'woman5.jpg' },
                 { image: 'woman6.jpg' }
             ],
-            stimulusCss: { color: '#31940F', 'font-size': '2.3em' }
+            stimulusCss: { color: '#31940F', 'font-size': '2.3em', 'width': '250px' }
         },
         category2: {
             name: global.menLabels,
@@ -35,7 +35,7 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/iat10
                 { image: 'man5.jpg' },
                 { image: 'man6.jpg' }
             ],
-            stimulusCss: { color: '#31940F', 'font-size': '2.3em' }
+            stimulusCss: { color: '#31940F', 'font-size': '2.3em', 'width': '250px' }
         },
         attribute1: {
             name: global.disabledLabels,
